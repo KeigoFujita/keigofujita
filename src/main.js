e8551492a463856cb3e9bf41276ab27ce2e341f3
Keigo Fujita <keigofujita19@gmail.com>
2021-05-01 15:15:14 +0800

    Auto stash before rebase of "refs/heads/master"

--- src/main.js
+++ src/main.js
@@ -3,9 +3,3 @@ import App from "./App.vue";
 import router from "./router";
 import "./assets/css/app.css";
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount("#app");
